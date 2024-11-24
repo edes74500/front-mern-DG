4e4072ee16cfdbe6cbba46b333412d5ed3865138
Jean-Daniel KLEIN <kleinjeandaniel@gmail.com>
2024-11-24 22:49:42 +0100

    1

--- src/app/api/apiSlice.tsx
+++ src/app/api/apiSlice.tsx
@@ -2,6 +2,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "https://localhost:3500" }),
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => "/users", // L'URL de l'API pour récupérer les utilisateurs
+      providesTags: ["User"], // Indique que ce endpoint est lié au tag "User"
+    }),
+  }),
   tagTypes: ["User", "Note"],
 });
