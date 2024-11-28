db495fbafd58b0f9b33cb48f08eb4593f14063cd
kleinjeandaniel <kleinjeandaniel@gmail.com>
2024-11-28 19:53:32 +0100

    aa

--- src/app/api/apiSlice.tsx
+++ src/app/api/apiSlice.tsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://api.klje7149.odns.fr/",
+    baseUrl: "http://api.passionanim.fr",
     validateStatus: (response, result) => {
       return response.status >= 200 && response.status < 300 && !result.isError;
     },
